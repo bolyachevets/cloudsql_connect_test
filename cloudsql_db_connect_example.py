22698013746d0aa512b5f9b8541b9cf375a16a25
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-11-01 16:12:11 -0700

    expriments with cloud sql db connectivity

---
+++ cloudsql_db_connect_example.py
@@ -0,0 +1,56 @@
+# Copyright © 2023 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+from google.cloud.sql.connector import Connector
+
+
+# locally works with Application Default Credentials (ADC) - i.e. gcloud login
+# remotely  works by setting GOOGLE_APPLICATION_CREDENTIALS to service account path
+# to test account locally - os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'auth-dev-sa-token.json'
+def cloudsql_db_connect():
+    try:
+        # Initialize the Cloud SQL Connector
+        connector = Connector()
+
+        # Retrieve connection details from environment variables
+        instance_connection_name = os.environ['DB_INSTANCE_CONNECTION_NAME']  # e.g., "project:region:instance"
+        username = os.environ['DB_USER']
+        password = os.environ['DB_PASSWORD']
+        dbname = os.environ['DB_DBNAME']
+
+        connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=username,
+            password=password,
+            db=dbname
+        )
+
+        # Create a cursor to execute a simple query
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()
+        print("Database version:", db_version[0])
+
+    except Exception as error:
+        print(f"An error occurred: {error}")
+    finally:
+        # Close the connection and connector
+        if connection:
+            connection.close()
+        connector.close()
+
+
+if __name__ == '__main__':
+    cloudsql_db_connect()
---
+++ run.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+cd /opt/app-root
+echo 'run cloudsql_db_connect_example.py'
+python cloudsql_db_connect_example.py
